2a972db057232ce0a790c39b492e89d0260e23ba
reissefer <reissefer@gmail.com>
2015-02-10 15:50:51 +0000

    started authorization and putting to couch commented structure of the put request.

--- server.js
+++ server.js
@@ -9,19 +9,31 @@ server.listen(8080, function() {
     console.log('incoming request being handled');
     server.put(/^\/lists\/([a-z]+)$/, function(req, res, next) {
         console.log('PUT ' + req.params[0]);
+        console.log('PUT ' + req.headers);
+        // checks to see if the username is in the URL 
         if(req.params[0] != req.authorization.basic.username) {
             return next(new restify.ForbiddenError('mismatched username and url'));
         }
+        // checks it contains  content type application/json
         if(req.headers['content-type'] != 'application/json') {
-            return next(new restify.UnsupportedMediaTypeError('bad Content-Type'));
+            return next(new restify.UnsupportedMediaTypeError('Bad Content-Type'));
         }
+        // checks if it has basic authorization
         if(req.authorization.scheme != 'Basic') {
             return next(new restify.UnauthorizedError('Basic HTTP auth required'));
         }
         console.log('parameters supplied');
-        var url = 'http://period-brave.codio.io:5984/lists/' + req.params[0];
+        var url = 'http://localhost:5984/lists/' + req.params[0];
+        // if put has items in the json it will grab whats in it, in this case items must be
+        // an array which could ( ["item","item", "item"])
+        console.log("items: " + req.params['items'])
+        req.params['items'].forEach(function(item){
+            console.log(item)
+        });
         var items = req.params['items'];
         request.get(url, function(err, response, body) {
+            console.log("request started")
+            // if the document isnt found it will create it from sratch
             if(response.statusCode == 404) {
                 console.log('document not found');
                 var salt = rand(160, 36);
@@ -67,7 +79,6 @@ server.listen(8080, function() {
                     return next(new restify.ForbiddenError('invalid username/password'));
                 }
                 console.log('passwords match!');
-                console.log('passwords match!');
                 var d = new Date();
                 var date = d.toUTCString()
                 body.last_modified = date;
@@ -90,7 +101,6 @@ server.listen(8080, function() {
                     res.send({
                         items: req.params['items']
                     });
-                    
                     res.end();
                 });
             };
