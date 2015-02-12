f8ed390befc6a9d6d6009d002a36675d4e2793f9
DavidFH <mrdavemv@gmail.com>
2015-02-12 16:57:31 +0000

    Code clean

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-//Adam testing git is working on the VPS
-//Were also not using Python hahaha no #####
-var restify = require('restify');
-var request = require('request');
-var rand = require('csprng');
-var sha1 = require('sha1');
-var server = restify.createServer();
+// Adam testing git is working on the VPS
+// Were also not using Python hahaha no #####
+var restify = require('restify'),
+    request = require('request'),
+    rand = require('csprng'),
+    sha1 = require('sha1'),
+    server = restify.createServer();
+// Setting server dependancys
 server.use(restify.CORS({
     origins: ['*'], // defaults to ['*']
     credentials: true, // defaults to false
@@ -15,50 +16,45 @@ server.use(restify.bodyParser());
 server.use(restify.queryParser());
 server.use(restify.authorizationParser());
 restify.CORS.ALLOW_HEADERS.push('authorization');
+// Creating Server
 server.listen(8080, function() {
-    console.log('incoming request being handled');
-    // lists put requests use as reference
-    
-    //use this as adding points
-    //The way this works is by having there name in at the moment e.g DOMAIN/checkin?user=USERNAME&location=7817587295719 This will then add 10 points at the moment
-    //we can change the amount places wanna do check in points at eventually. 
+    console.log('Incoming request being handled.');
+    // The way this works is by having there name in at the moment e.g DOMAIN/checkin?user=USERNAME&location=7817587295719 This will then add 10 points at the moment
     server.put("/checkin", function(req, res, next) {
-        var user = req.query.user
-        // this will stop the rest of the request from carrying on, user contains the username of the person
-        //return next(new restify.ForbiddenError(user));
+        // Get user and set couchdb url
+        var user = req.query.user,
+            url = 'http://localhost:5984/users/' + user;
         console.log('CHECKIN ');
         console.log('PUT ' + user);
         // checks to see if the username is the same as the one in the URL 
         if(user != req.authorization.basic.username) {
-            return next(new restify.ForbiddenError('You cant access that user'));
+            return next(new restify.ForbiddenError('You cant access that user.'));
         }
         // checks it contains  content type application/json
         if(req.headers['content-type'] != 'application/json') {
-            return next(new restify.UnsupportedMediaTypeError('Bad Content-Type'));
+            return next(new restify.UnsupportedMediaTypeError('Bad Content-Type.'));
         }
         // checks if it has basic authorization
         if(req.authorization.scheme != 'Basic') {
-            return next(new restify.UnauthorizedError('Basic HTTP auth required'));
+            return next(new restify.UnauthorizedError('Basic HTTP auth required.'));
         }
-        console.log('parameters supplied');
-        var url = 'http://localhost:5984/users/' + user
-        // we have the url for the couchDB so now to get the users profile
+        console.log('Parameters supplied.');
         request.get(url, function(err, response, body) {
-            console.log("request started")
+            console.log("Request started.")
             // if the document isnt found it will create it from sratch
             if(response.statusCode == 404) {
                 return next(new restify.ForbiddenError('User Not Found'));
             };
             if(response.statusCode == 200) {
-                console.log('existing document');
+                console.log('Existing document.');
                 body = JSON.parse(body);
                 var pwd = sha1(req.authorization.basic.password + body.salt);
                 if(pwd != body.password) {
-                    return next(new restify.ForbiddenError('invalid username/password'));
+                    return next(new restify.ForbiddenError('Invalid username/password.'));
                 }
-                console.log('passwords match!');
-                var d = new Date();
-                var date = d.toUTCString()
+                console.log('Passwords match!');
+                var d = new Date(),
+                    date = d.toUTCString();
                 // change what we need in the body e.g the points can probably add to the array aswell
                 body.last_modified = date;
                 body.points = body.points + 10;
@@ -75,7 +71,6 @@ server.listen(8080, function() {
                     }
                     // document has been inserted into database
                     res.setHeader('Location', 'http://' + req.headers.host + req.url);
-                    res.setHeader('ETag', body.rev);
                     res.setHeader('Last-Modified', date);
                     res.setHeader('Content-Type', 'application/json');
                     res.setHeader('Accepts', 'PUT');
