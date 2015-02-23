de2f2dbbbb7d5d6441633c67fade016ed5fad888
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-23 14:05:56 +0000

    validateHTTP

--- validateHTTP.js
+++ validateHTTP.js
@@ -7,6 +7,7 @@ var restify = require('restify'),
 function validateHTTP(req, res, next) {
     // checks to see if the username is in the URL 
     if (req.params[0] != req.authorization.basic.username) {
+        console.log(req.params = 'req.params in validateHTTP')
         return next(new restify.ForbiddenError('You can\'t access that user'));
     }
     // checks it contains  content type application/json
