0130862c51d4cc2e0c03bc3c17351f16cccd4c59
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-23 11:53:50 +0000

    Module test

---
+++ getuser.js
@@ -0,0 +1,40 @@
+var validateHTTP = require("./validateHTTP.js");
+
+function getuser(req, res, next) {
+    //Grab a users profile
+    console.log('GRABBING USER');
+    console.log('GET ' + req.params[0]);
+    var user = {
+        test: 'test'
+    };
+    validateHTTP.validateHTTP(req, res, next)
+    console.log('parameters supplied');
+    var url = 'http://localhost:5984/users/' + req.params[0];
+    request.get(url, function(err, response, body) {
+        console.log("request started")
+        // if user is not found will send 404 error
+        if(response.statusCode == 404) {
+            return next(new restify.BadRequestError('User Not Found'))
+        };
+        if(response.statusCode == 200) {
+            body = JSON.parse(body);
+            //res.header('ETag', body._rev);
+            res.header('Last-Modified', body.last_modified);
+            res.header('Accepts', 'GET');
+            user = {
+                id: body._id,
+                date_joined: body.date_joined,
+                last_modified: body.last_modified,
+                points: body.points,
+                transactions: body.transactions
+            }
+            console.log(user.points)
+            console.log("set user");
+            res.send(user);
+            console.log("sent data");
+            res.end();
+        };
+    });
+};
+
+module.exports.getuser = getuser;
---
+++ register.js
@@ -0,0 +1,50 @@
+function register(req, res, next) {
+    console.log('NEW USER!');
+    console.log('PUT: ' + req.params[0])
+    var url = 'http://localhost:5984/users/' + req.params[0];
+    validateHTTP.validateHTTP(req, res, next)
+    request.get(url, function(err, response, body) {
+        if(err) {
+            return next(new restify.InternalServerError('Error has occured'));
+        }
+        // if the document isnt found it will create it from sratch
+        console.log('code' + response.statusCode)
+        if(response.statusCode == 200) {
+            return next(new restify.InternalServerError('user already created'));
+        } else if(response.statusCode == 404) {
+            var salt = rand(160, 36),
+                password = sha1(req.authorization.basic.password + salt),
+                d = new Date(),
+                date = d.toUTCString();
+            console.log(date)
+            var doc = {
+                date_joined: date,
+                last_modified: date,
+                password: password,
+                salt: salt,
+                points: 0,
+                transactions: []
+            };
+            var docStr = JSON.stringify(doc);
+            var params = {
+                uri: url,
+                body: JSON.stringify(doc)
+            };
+            request.put(params, function(err, response, body) {
+                if(err) {
+                    return next(new restify.InternalServerError('Cant create document'));
+                }
+                // document has been inserted into database
+                body = JSON.parse(body);
+                res.send({
+                    user: req.params
+                });
+                res.end();
+            });
+        };
+    });
+    // if the document is found, that means the user is already created.
+};
+
+module.exports.register = register;
+
