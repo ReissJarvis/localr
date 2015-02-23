5289632dd3a176e098d2e7c3db7a485c1696e291
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-23 14:40:00 +0000

    Changed mapping of URLs

--- debug_and_test/js/debug.js
+++ debug_and_test/js/debug.js
@@ -8,7 +8,7 @@ localr = (function() {
     return {
         getUser: function() {
             localr.setCred();
-            var url = 'http://178.62.31.30/users?username="' + username + '"'
+            var url = 'http://178.62.31.30/users?username=' + username
             if(window.XMLHttpRequest) { // mozilla, safari,...
                 httpRequest = new XMLHttpRequest();
             } else if(window.ActiveXObject) {
@@ -30,7 +30,7 @@ localr = (function() {
         },
         register: function() {
             localr.setCred();
-            var url = 'http://178.62.31.30/register?username"' + username + '"'
+            var url = 'http://178.62.31.30/register?username=' + username 
             if(window.XMLHttpRequest) { // mozilla, safari,...
                 httpRequest = new XMLHttpRequest();
             } else if(window.ActiveXObject) {
@@ -62,7 +62,7 @@ localr = (function() {
         },
         checkin: function(){
             var points = document.getElementById("points").value
-            var url = 'http://178.62.31.30/checkin?user=' + username + '&points=' + points;
+            var url = 'http://178.62.31.30/checkin?username=' + username + '&points=' + points;
             if(window.XMLHttpRequest) { // mozilla, safari,...
                 httpRequest = new XMLHttpRequest();
             } else if(window.ActiveXObject) {
