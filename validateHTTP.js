8cad756aa6292b42107124c41e1b58d7cd58d3ec
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-23 13:43:31 +0000

    validateHTTP finally working - was to do with the req.params being passed as object and not array

--- validateHTTP.js
+++ validateHTTP.js
@@ -6,9 +6,7 @@ var restify = require('restify'),
 
 function validateHTTP(req, res, next) {
     // checks to see if the username is in the URL 
-    if (req.params.user != req.authorization.basic.username) {
-        console.log(req.params.user)
-        console.log('req.params0 = ' + req.params[0] + ' basic.username = ' + req.authorization.basic.username)
+    if (req.params.user !== req.authorization.basic.username) {
         return next(new restify.ForbiddenError('You can\'t access that user'));
     }
     // checks it contains  content type application/json
