0cf8e916fc743479021c9260f1f0cb5f631e09ce
adam91holt <holta5@uni.coventry.ac.uk>
2015-02-24 14:21:43 +0000

    Testing if statement for validateHTTP

--- checkin.js
+++ checkin.js
@@ -14,53 +14,51 @@ function checkin(req, res, next) {
     console.log('PUT ' + user);
     console.log('Points = ' + points);
     console.log('Parameters supplied.');
-    
-    request.get(url, function (err, response, body) {
-        console.log('in checkin.js'+req.params[0]);
-        validateHTTP.validateHTTP(req, res, next);
-        console.log("Request started.");
-        // if the document isnt found it will create it from sratch
-        if (response.statusCode === 404) {
-            return next(new restify.ForbiddenError('User Not Found'));
-        } ;
-        if (response.statusCode === 200) {
-            console.log('Existing document.');
-            body = JSON.parse(body);
-            var pwd = sha1(req.authorization.basic.password + body.salt);
-            if(pwd != body.password) {
-                return next(new restify.ForbiddenError('Invalid username/password.'));
-            }
-            console.log('Passwords match!');
-            var d = new Date(),
-                date = d.toUTCString();
-            // change what we need in the body e.g the points can probably add to the array aswell
-            body.last_modified = date;
-            body.points = body.points + points;
-            // adding the transactions to the array so we can keep track of them
-            body.transactions.push({
-                transactionid: uuid.v1(),
-                date: date,
-                amount_of_points: points
-            })
-            console.log(body.points);
-            var params = {
-                uri: url,
-                body: JSON.stringify(body)
+    request.get(url, function(err, response, body) {
+        if((validateHTTP.validateHTTP(req, res, next)) === true) {
+            console.log("Request started.");
+            // if the document isnt found it will create it from sratch
+            if(response.statusCode === 404) {
+                return next(new restify.ForbiddenError('User Not Found'));
             };
-            request.put(params, function (err, response, body) {
-                if (err) {
-                    return next(new restify.InternalServerError('Cant create document'));
+            if(response.statusCode === 200) {
+                console.log('Existing document.');
+                body = JSON.parse(body);
+                var pwd = sha1(req.authorization.basic.password + body.salt);
+                if(pwd != body.password) {
+                    return next(new restify.ForbiddenError('Invalid username/password.'));
                 }
-                // document has been inserted into database
-                res.setHeader('Location', 'http://' + req.headers.host + req.url);
-                res.setHeader('Last-Modified', date);
-                res.setHeader('Content-Type', 'application/json');
-                res.setHeader('Accepts', 'PUT');
-                res.send("points added");
-                res.end();
-            });
-        };
-    });
+                console.log('Passwords match!');
+                var d = new Date(),
+                    date = d.toUTCString();
+                // change what we need in the body e.g the points can probably add to the array aswell
+                body.last_modified = date;
+                body.points = body.points + points;
+                // adding the transactions to the array so we can keep track of them
+                body.transactions.push({
+                    transactionid: uuid.v1(),
+                    date: date,
+                    amount_of_points: points
+                })
+                console.log(body.points);
+                var params = {
+                    uri: url,
+                    body: JSON.stringify(body)
+                };
+                request.put(params, function(err, response, body) {
+                    if(err) {
+                        return next(new restify.InternalServerError('Cant create document'));
+                    }
+                    // document has been inserted into database
+                    res.setHeader('Location', 'http://' + req.headers.host + req.url);
+                    res.setHeader('Last-Modified', date);
+                    res.setHeader('Content-Type', 'application/json');
+                    res.setHeader('Accepts', 'PUT');
+                    res.send("points added");
+                    res.end();
+                });
+            };
+        }
+    })
 };
-
 module.exports.checkin = checkin;
--- validateHTTP.js
+++ validateHTTP.js
@@ -5,17 +5,26 @@ var restify = require('restify'),
     uuid = require('node-uuid');
 
 function validateHTTP(req, res, next) {
+    var failed = false;
     // checks to see if the username is in the URL 
     if (req.params.username != req.authorization.basic.username) {
         return next(new restify.ForbiddenError('You can\'t access that user'));
+        failed = true;
     }
     // checks it contains  content type application/json
     if (req.headers['content-type'] !== 'application/json') {
         return next(new restify.UnsupportedMediaTypeError('Bad Content-Type'));
+        failed = true;
     }
     // checks if it has basic authorization
     if (req.authorization.scheme !== 'Basic') {
         return next(new restify.UnauthorizedError('Basic HTTP auth required'));
+        failed = true;
+    }
+    if (failed === true) {
+        return false
+    } else {
+        return true
     }
 }
 
