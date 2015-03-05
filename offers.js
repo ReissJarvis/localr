6ae0ac42af8f9fadff3ab93ead5261e948b5cfa4
adam91holt <holta5@uni.coventry.ac.uk>
2015-03-05 14:53:50 +0000

    Fixed error where I forgot a +

--- offers.js
+++ offers.js
@@ -95,7 +95,7 @@ module.exports.getBusinessOffers = function(req, res, next) {
         console.log('Get All OFFERS!');
         var business = req.params.businessname;
         //Gets all offers from couchDB as JSON
-        var url = "http://localhost:5984/offers/_design/offers/_view/business?startkey='" + business + "'&endkey='" + business "'";
+        var url = "http://localhost:5984/offers/_design/offers/_view/business?startkey='" + business + "'&endkey='" + business + "'";
         request.get(url, function(err, response, body) {
             if(err) {
                 return next(new restify.InternalServerError('Error has occured'));
