d5d17c3df96b20ab3c6e97a746aa6a72959155a0
DavidFH <mrdavemv@gmail.com>
2015-03-05 15:19:36 +0000

    fixed node async errors

--- del.js
+++ del.js
@@ -27,39 +27,39 @@ function del(req, res, next, type) {
                         nodeid = body.nodeid;
                     console.log(rev);
                     console.log(nodeid);
-                    db.cypherQuery("start m = node("+ nodeid +") match n<-[r]-m  return r", function(err, results) {
+                    db.cypherQuery("start m = node(" + nodeid + ") match n<-[r]-m  return r", function(err, results) {
                         if(err) throw err;
                         console.log(results);
-                        results.data.forEach(function(item){
+                        results.data.forEach(function(item) {
                             var id = item._id;
                             console.log("Loggin id: " + id);
-                            db.deleteRelationship(id, function(err, relationship){
+                            db.deleteRelationship(id, function(err, relationship) {
                                 if(err) throw err;
-                                if(relationship === true){
+                                if(relationship === true) {
                                     console.log("Deleted Relationship!");
+                                    db.deleteNode(nodeid, function(err, node) {
+                                        if(err) throw err;
+                                        if(node === true) {
+                                            console.log("Node Deleted!");
+                                            request.del(url + "?rev=" + rev, function(err, response) {
+                                                if(err) {
+                                                    return next(new restify.InternalServerError('Cant delete document'));
+                                                };
+                                                console.log("Deleted User!");
+                                                res.send("Deleted User!");
+                                                res.end();
+                                            });
+                                        } else {
+                                            console.log("Node not Deleted");
+                                            return next(new restify.InternalServerError('Cant delete node!'));
+                                        };
+                                    });
                                 } else {
                                     console.log("Relationship not Deleted...");
                                     return next(new restify.InternalServerError('Cant delete Relationship!'));
                                 };
                             });
                         });
-                    });
-                    b.deleteNode(nodeid, function(err, node) {
-                        if(err) throw err;
-                        if(node === true) {
-                            console.log("Node Deleted!");
-                        } else {
-                            console.log("Node not Deleted");
-                            return next(new restify.InternalServerError('Cant delete node!'));
-                        };
-                    });
-                    request.del(url + "?rev=" + rev, function(err, response) {
-                        if(err) {
-                            return next(new restify.InternalServerError('Cant delete document'));
-                        };
-                        console.log("Deleted User!");
-                        res.send("Deleted User!");
-                        res.end();
                     });
                 };
             });
