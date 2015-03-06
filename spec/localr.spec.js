e7fc06bfe2fcec216302543358e200ba08121eb4
reissefer <reissefer@gmail.com>
2015-03-06 17:11:14 +0000

    jasmine adds a user, checks for duplicate and then deletes it

--- spec/localr.spec.js
+++ spec/localr.spec.js
@@ -1,251 +1,120 @@
+ var request = require('request');
 describe('Localr API', function() {
-    var request = require('request');
+   
     describe('Users', function() {
         describe('create the user', function(done) {
+            //             beforeEach(function() {
+            //                 var restify = require('restify');
+            //                 var test = require('../register.js');
+            //                 var server = restify.createServer();
+            //                 test.init(server);
+            //                 server.listen('8080', 'localhost', function() {
+            //                     console.log('%s server listen at %s', server.name, server.url);
+            //                 });
+            //             })
             it("be able to create a user", function(done) {
-                  
                 var url = 'http://api.adam-holt.co.uk/users/testuser';
                 var doc = {
-                            name:"testuser"
-                        };
+                    name: "testuser"
+                };
+                // getting params
                 var params = {
-                            uri: url,
-                            body: JSON.stringify(doc)
-                        };
+                    uri: url,
+                    headers:{
+                        authorization: getBasic('testuser','test')
+                    },
+                    body: JSON.stringify(doc)
+                };
+                console.log(params);
+                console.log('testing post')
                 request.post(params, function(error, response, body) {
-                    expect(response.statusCode).toBe(201);
-                    expect(response.body).toBe(true);
+                    expect(response.statusCode).toBe(200);
+                    if(error){
+                    expect(error.code).not.toBe('ECONNREFUSED');
+                }
+                    done();
+                })
+            })
+            it("be not able to create duplicate user", function(done) {
+                 var url = 'http://api.adam-holt.co.uk/users/testuser';
+                var doc = {
+                    name: "testuser"
+                };
+                // getting params
+                var params = {
+                    uri: url,
+                    headers:{
+                        authorization: getBasic('testuser','test')
+                    },
+                    body: JSON.stringify(doc)
+                };
+                console.log(params);
+                console.log('testing post')
+                request.post(params, function(error, response, body) {
+                    expect(response.statusCode).toBe(409);
+                    if(error){
+                    expect(error.code).not.toBe('ECONNREFUSED');
+                }
+                    done();
                 })
             })
         })
     })
-    describe('checkin', function() {
-        it("be able to checkin", function() {})
+    describe('checkin', function(done) {
+        it("be able to checkin", function(done) {
+            var url = 'http://api.adam-holt.co.uk/users/testuser';
+                var doc = {
+                    name: "testuser"
+                };
+                // getting params
+                var params = {
+                    uri: url,
+                    headers:{
+                        authorization: getBasic('testuser','test')
+                    },
+                    body: JSON.stringify(doc)
+                };
+                console.log(params);
+                console.log('testing post')
+                request.post(params, function(error, response, body) {
+                    expect(response.statusCode).toBe(409);
+                    if(error){
+                    expect(error.code).not.toBe('ECONNREFUSED');
+                }
+                    done();
+                })
+        })
         it("check points have been added", function() {})
     })
     it("be able to see where you've checked in", function() {})
-    it("be able delete yourself", function() {})
+    it("be able delete yourself", function(done) {
+        var url = 'http://api.adam-holt.co.uk/users/testuser';
+                // getting params
+                console.log('at delete')
+                var params = {
+                    uri: url,
+                    headers:{
+                        authorization: getBasic('testuser','test')
+                    },
+                };
+                console.log(params);
+                console.log('testing post')
+                request.del(params, function(error, response, body) {
+                    expect(response.statusCode).toBe(200);
+                    if(error){
+                    expect(error.code).not.toBe('ECONNREFUSED');
+                }
+                    done();
+                })
+        
+    })
+    it("be able to create a group", function() {})
+    it("be able to delete a group", function() {})
+    it("be able to join a group", function() {})
+    it("Be able to get the latest offers", function() {})
+    it("be able to redeem the latest offer", function() {})
+    it("be able to check what offers youve redeemed", function() {})
 });
-it("be able to create a group", function() {})
-it("be able to delete a group", function() {})
-it("be able to join a group", function() {})
-it("Be able to get the latest offers", function() {})
-it("be able to redeem the latest offer", function() {})
-it("be able to check what offers youve redeemed", function() {})
-});
-getBasic = function(username, password) {
-    return "Basic " + btoa(username + ":" + password);
-}
-//  getDetails: function(type) {
-//             if(type == "users") {
-//                 localr.setCred("users");
-//                 var url = 'http://api.adam-holt.co.uk/users/get?username=' + name;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('GET', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('Content-Type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log('In the get req');
-//                         console.log(response);
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else if(type == "business") {
-//                 localr.setCred("business");
-//                 var url = 'http://api.adam-holt.co.uk/business/get?businessname=' + name;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('GET', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('Content-Type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log('In the get req');
-//                         console.log(response);
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else {
-//                 console.log("Error, Invalid Type!");
-//             };
-//         },
-//         register: function(type) {
-//             if(type == "users") {
-//                 localr.setCred("users");
-//                 var url = 'http://api.adam-holt.co.uk/users/register?username=' + name;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('PUT', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('content-type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log(response);
-//                         return response, httpRequest;
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                         return httpRequest;
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else if(type == "business") {
-//                 localr.setCred("business");
-//                 var url = 'http://api.adam-holt.co.uk/business/register?businessname=' + name;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('PUT', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('content-type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log(response);
-//                         return response
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                         return httpRequest.statusText
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else {
-//                 console.log("Error, Invalid Type!");
-//             };
-//         },
-//         setCred: function(type) {
-//             if(type == "users") {
-//                 name = document.getElementById("username").value
-//                 console.log(name);
-//                 password = document.getElementById("userpassword").value
-//                 console.log(password);
-//                 localr.getBasic();
-//             } else if(type == "business") {
-//                 name = document.getElementById("businessname").value;
-//                 console.log(name);
-//                 password = document.getElementById("businesspassword").value;
-//                 console.log(password);
-//                 localr.getBasic();
-//             } else {
-//                 console.log("Error, Invalid Type!");
-//             };
-//         },
-//         getBasic: function() {
-//             credentials = "Basic " + btoa(name + ":" + password);
-//             console.log(credentials);
-//         },
-//         checkin: function() {
-//             var points = document.getElementById("points").value
-//             var url = 'http://api.adam-holt.co.uk/users/checkin?username=' + name + '&points=' + points;
-//             if(window.XMLHttpRequest) { // mozilla, safari,...
-//                 httpRequest = new XMLHttpRequest();
-//             } else if(window.ActiveXObject) {
-//                 httpRequest = ("Microsoft.XMLHTTP");
-//             }
-//             httpRequest.open('PUT', url);
-//             httpRequest.setRequestHeader('Authorization', credentials);
-//             httpRequest.setRequestHeader('content-type', "application/json");
-//             httpRequest.onload = function() {
-//                 if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                     var response = JSON.parse(httpRequest.responseText);
-//                     console.log(response)
-//                     return response
-//                 } else {
-//                     console.log(httpRequest.statusText);
-//                     return httpRequest.statusText
-//                 }
-//             };
-//             httpRequest.send();
-//         },
-//         delete: function(type) {
-//             if(type == "users") {
-//                 localr.setCred("users");
-//                 var url = 'http://api.adam-holt.co.uk/users/delete?username=' + name;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('DELETE', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('Content-Type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log('In the delete req');
-//                         console.log(response);
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else if(type == "business") {
-//                 localr.setCred("business");
-//                 var url = 'http://api.adam-holt.co.uk/business/delete?businessname=' + name;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('GET', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('Content-Type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log('In the get req');
-//                         console.log(response);
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else {
-//                 console.log("Error, Invalid Type!");
-//             }
-//         },
-//         addOffer: function(type) {
-//             var offer = document.getElementById("offertitle").value;
-//             var description = document.getElementById("offerdescription").value;
-//             if(type == "business") {
-//                 localr.setCred("business");
-//                 var url = 'http://api.adam-holt.co.uk/business/offers/add?businessname=' + name + '&offer=' + offer + '&description=' + description;
-//                 if(window.XMLHttpRequest) { // mozilla, safari,...
-//                     httpRequest = new XMLHttpRequest();
-//                 } else if(window.ActiveXObject) {
-//                     httpRequest = ("Microsoft.XMLHTTP");
-//                 }
-//                 httpRequest.open('PUT', url);
-//                 httpRequest.setRequestHeader('Authorization', credentials);
-//                 httpRequest.setRequestHeader('content-type', "application/json");
-//                 httpRequest.onload = function() {
-//                     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-//                         var response = JSON.parse(httpRequest.responseText);
-//                         console.log(response);
-//                     } else {
-//                         console.log(httpRequest.statusText);
-//                     }
-//                 };
-//                 httpRequest.send();
-//             } else {
-//                 console.log("Error, Invalid Type!");
-//             };
+getBasic = function(username, password){
+    return "Basic " + new Buffer(username + ":" + password).toString('base64')
+};
