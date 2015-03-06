a4fcce28b25717a0106e61260b58049401dbb887
adam91holt <holta5@uni.coventry.ac.uk>
2015-03-06 19:02:46 +0000

    Changed creating an offer so it takes the data from the body rather than through the url

--- offers.js
+++ offers.js
@@ -6,68 +6,65 @@ var restify = require('restify'),
     uuid = require('node-uuid'),
     neo4j = require('node-neo4j');
 module.exports.addOffer = function(req, res, next) {
-    if((validateHTTP.validateHTTP(req, res, next, "business")) === true) {
-        console.log('NEW OFFER!');
-        console.log('PUT: ' + req.params.offer)
-        db = new neo4j('http://localhost:7474');
-        var rand = uuid.v1()
-        var businessName = req.params.businessname
-        var offertitle = req.params.offer + ' - ' + businessName
-        var nodeid = 0;
-        var url = 'http://localhost:5984/offers/' + offertitle;
-        var description = req.params.description
-        var REQ = req
-        request.get(url, function(err, response, body) {
-            if(err) {
-                return next(new restify.InternalServerError('Error communicating with CouchDB'));
-            }
-            // if the document isnt found it will create it from sratch
-            console.log('code' + response.statusCode)
-            if(response.statusCode === 200) {
-                return next(new restify.ConflictError('Offer has already been created with the same name'));
-            } else if(response.statusCode === 404) {
-                db.insertNode({
-                    name: offertitle
-                }, ['Offer', businessName], function(err, node) {
-                    if(err) throw err;
-                    // Output node properties.
-                    console.log('New neo4j node created with name = ' + node.name);
-                    // Output node id.
-                    console.log(node._id);
-                    nodeid = node._id
-                    var d = new Date(),
-                        date = d.toUTCString();
-                    console.log(date);
-                    console.log('Just above doc node id  ' + nodeid);
-                    var doc = {
-                        date_created: date,
-                        last_modified: date,
-                        uuid: rand,
-                        offer_title: REQ.params.offer,
-                        offer_description: description,
-                        businessname: businessName,
-                        redeems: [],
-                        nodeid: nodeid
-                    };
-                    console.log('underneath node.id  ' + doc.nodeid);
-                    var docStr = JSON.stringify(doc);
-                    var params = {
-                        uri: url,
-                        body: docStr
-                    };
-                    request.put(params, function(err, response, body) {
-                        if(err) {
-                            return next(new restify.InternalServerError('Cant create document in CouchDB'));
-                        }
-                        // document has been inserted into database
-                        res.send(docStr);
-                        res.end();
-                    });
+    //Data that has been sent in the request
+    var body = JSON.parse(req.body);
+    console.log('NEW OFFER!');
+    //Connection to neo4j
+    db = new neo4j('http://localhost:7474');
+    var rand = uuid.v1()
+    var businessName = body.businessname
+    var description = body.description
+    var offertitle = body.title + ' - ' + businessName
+    var nodeid = 0;
+    //URL for when offer will be stored in CouchDB
+    var url = 'http://localhost:5984/offers/' + offertitle;
+    request.get(url, function(err, response, body) {
+        if(err) {
+            return next(new restify.InternalServerError('Error communicating with CouchDB'));
+        }
+        // if the document isnt found it will create it from sratch
+        console.log('CouchDB request statuscode - ' + response.statusCode)
+        if(response.statusCode === 200) {
+            return next(new restify.ConflictError('Offer has already been created with the same name'));
+        } else if(response.statusCode === 404) {
+            //Insert node into neo4j
+            db.insertNode({
+                name: offertitle
+            }, ['Offer', businessName], function(err, node) {
+                if(err) throw err;
+                // Output node properties.
+                console.log('New neo4j node created with name = ' + node.name);
+                // Output node id.
+                console.log(node._id);
+                nodeid = node._id
+                var d = new Date(),
+                    date = d.toUTCString();
+                var doc = {
+                    date_created: date,
+                    last_modified: date,
+                    offer_title: offertitle,
+                    offer_description: description,
+                    businessname: businessName,
+                    redeems: [],
+                    nodeid: nodeid
+                };
+                var docStr = JSON.stringify(doc);
+                var params = {
+                    uri: url,
+                    body: docStr
+                };
+                request.put(params, function(err, response, body) {
+                    if(err) {
+                        return next(new restify.InternalServerError('Cant create document in CouchDB'));
+                    }
+                    // document has been inserted into database
+                    // now send url and document back to user
+                    res.send(docStr);
+                    res.end();
                 });
-            };
-        });
-        // if the document is found, that means the user is already created.
-    };
+            });
+        };
+    });
 };
 module.exports.getAllOffers = function(req, res, next) {
     console.log('Get All OFFERS!');
