dee801a6629a90ca0efad2f346201c62b065ffea
adam91holt <holta5@uni.coventry.ac.uk>
2015-03-06 13:55:22 +0000

    Changed routing for register and delete

--- register.js
+++ register.js
@@ -14,59 +14,59 @@ function register(req, res, next, type) {
         var nodeid = 0;
         var url = 'http://localhost:5984/users/' + req.params.username;
         request.get(url, function(err, response, body) {
-                if(err) {
-                    return next(new restify.InternalServerError('Could not get user from CouchDB'));
-                }
-                // if the document isnt found it will create it from sratch
-                console.log('code' + response.statusCode);
-                if(response.statusCode === 200) {
-                    return next(new restify.ConflictError('user already created'));
-                } else if(response.statusCode === 404) {
-                    db.insertNode({
-                        name: req.params.username
-                    }, ['User'], function(err, node) {
-                        if(err) throw err;
-                        // Output node properties.
-                        console.log('New neo4j node created with name = ' + node.name);
-                        // Output node id.
-                        console.log(node._id);
-                        nodeid = node._id
-                        var salt = rand(160, 36),
-                            password = sha1(req.authorization.basic.password + salt),
-                            d = new Date(),
-                            date = d.toUTCString();
-                        console.log(date);
-                        console.log('Just above doc node id  ' + nodeid);
-                        var doc = {
-                            date_joined: date,
-                            last_modified: date,
-                            password: password,
-                            salt: salt,
-                            points: 0,
-                            transactions: [],
-                            nodeid: nodeid
-                        };
-                        console.log('underneath node.id  ' + doc.nodeid);
-                        var docStr = JSON.stringify(doc);
-                        var params = {
-                            uri: url,
-                            body: JSON.stringify(doc)
-                        };
-                        request.put(params, function(err, response, body) {
-                            if(err) {
-                                return next(new restify.InternalServerError('Cant create document'));
-                            }
-                            // document has been inserted into database
-                            body = JSON.parse(body);
-                            res.send({
-                                user: req.params
-                            });
-                            res.end();
+            if(err) {
+                return next(new restify.InternalServerError('Could not get user from CouchDB'));
+            }
+            // if the document isnt found it will create it from sratch
+            console.log('code' + response.statusCode);
+            if(response.statusCode === 200) {
+                return next(new restify.ConflictError('user already created'));
+            } else if(response.statusCode === 404) {
+                db.insertNode({
+                    name: req.params.username
+                }, ['User'], function(err, node) {
+                    if(err) throw err;
+                    // Output node properties.
+                    console.log('New neo4j node created with name = ' + node.name);
+                    // Output node id.
+                    console.log(node._id);
+                    nodeid = node._id
+                    var salt = rand(160, 36),
+                        password = sha1(req.authorization.basic.password + salt),
+                        d = new Date(),
+                        date = d.toUTCString();
+                    console.log(date);
+                    console.log('Just above doc node id  ' + nodeid);
+                    var doc = {
+                        date_joined: date,
+                        last_modified: date,
+                        password: password,
+                        salt: salt,
+                        points: 0,
+                        transactions: [],
+                        nodeid: nodeid
+                    };
+                    console.log('underneath node.id  ' + doc.nodeid);
+                    var docStr = JSON.stringify(doc);
+                    var params = {
+                        uri: url,
+                        body: JSON.stringify(doc)
+                    };
+                    request.put(params, function(err, response, body) {
+                        if(err) {
+                            return next(new restify.InternalServerError('Cant create document'));
+                        }
+                        // document has been inserted into database
+                        body = JSON.parse(body);
+                        res.send({
+                            user: req.params
                         });
+                        res.end();
                     });
                 });
+            };
             // if the document is found, that means the user is already created.
-        };
+        });
     } else if(type == "business") {
         console.log('NEW BUSINESS!');
         console.log('PUT: ' + req.params.businessname);
@@ -74,57 +74,57 @@ function register(req, res, next, type) {
         var nodeid = 0;
         var url = 'http://localhost:5984/business/' + req.params.businessname;
         request.get(url, function(err, response, body) {
-                if(err) {
-                    return next(new restify.InternalServerError('Error has occured'));
-                }
-                // if the document isnt found it will create it from sratch
-                console.log('code' + response.statusCode);
-                if(response.statusCode === 200) {
-                    return next(new restify.ConflictError('user already created'));
-                } else if(response.statusCode === 404) {
-                    db.insertNode({
-                        name: req.params.businessname
-                    }, ['Business', req.params.businessname], function(err, node) {
-                        if(err) throw err;
-                        // Output node properties.
-                        console.log('New neo4j node created with name = ' + node.name);
-                        // Output node id.
-                        console.log(node._id);
-                        nodeid = node._id
-                        var salt = rand(160, 36),
-                            password = sha1(req.authorization.basic.password + salt),
-                            d = new Date(),
-                            date = d.toUTCString();
-                        console.log(date);
-                        console.log('Just above doc node id  ' + nodeid);
-                        var doc = {
-                            date_joined: date,
-                            last_modified: date,
-                            password: password,
-                            salt: salt,
-                            nodeid: nodeid
-                        };
-                        console.log('underneath node.id  ' + doc.nodeid);
-                        var docStr = JSON.stringify(doc);
-                        var params = {
-                            uri: url,
-                            body: JSON.stringify(doc)
-                        };
-                        request.put(params, function(err, response, body) {
-                            if(err) {
-                                return next(new restify.InternalServerError('Cant create document'));
-                            }
-                            // document has been inserted into database
-                            body = JSON.parse(body);
-                            res.send({
-                                business: req.params
-                            });
-                            res.end();
+            if(err) {
+                return next(new restify.InternalServerError('Error has occured'));
+            }
+            // if the document isnt found it will create it from sratch
+            console.log('code' + response.statusCode);
+            if(response.statusCode === 200) {
+                return next(new restify.ConflictError('user already created'));
+            } else if(response.statusCode === 404) {
+                db.insertNode({
+                    name: req.params.businessname
+                }, ['Business', req.params.businessname], function(err, node) {
+                    if(err) throw err;
+                    // Output node properties.
+                    console.log('New neo4j node created with name = ' + node.name);
+                    // Output node id.
+                    console.log(node._id);
+                    nodeid = node._id
+                    var salt = rand(160, 36),
+                        password = sha1(req.authorization.basic.password + salt),
+                        d = new Date(),
+                        date = d.toUTCString();
+                    console.log(date);
+                    console.log('Just above doc node id  ' + nodeid);
+                    var doc = {
+                        date_joined: date,
+                        last_modified: date,
+                        password: password,
+                        salt: salt,
+                        nodeid: nodeid
+                    };
+                    console.log('underneath node.id  ' + doc.nodeid);
+                    var docStr = JSON.stringify(doc);
+                    var params = {
+                        uri: url,
+                        body: JSON.stringify(doc)
+                    };
+                    request.put(params, function(err, response, body) {
+                        if(err) {
+                            return next(new restify.InternalServerError('Cant create document'));
+                        }
+                        // document has been inserted into database
+                        body = JSON.parse(body);
+                        res.send({
+                            business: req.params
                         });
+                        res.end();
                     });
                 });
-            // if the document is found, that means the user is already created.
-        };
+            }
+        });
+        // if the document is found, that means the user is already created.
     } else {
         console.log("Error, Invalid Type!");
     };
