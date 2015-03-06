88aa81a60f9ccb779f5f9c83cfbe56179791756d
adam91holt <holta5@uni.coventry.ac.uk>
2015-03-06 13:19:41 +0000

    Adding some server.on options

--- offers.js
+++ offers.js
@@ -102,7 +102,7 @@ module.exports.getAllOffers = function(req, res, next) {
 };
 module.exports.getBusinessOffers = function(req, res, next) {
     var business = req.params.businessname;
-    console.log('Get All OFFERS from' + business);
+    console.log('Get OFFERS from' + business);
     //Gets all offers from couchDB as JSON
     var url = 'http://localhost:5984/offers/_design/offers/_view/business?startkey="' + business + '"&endkey="' + business + '"';
     request.get(url, function(err, response, body) {
