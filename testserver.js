6c65aa384e70f05efa4a1d05e961941e2ca7e92e
reissefer <reissefer@gmail.com>
2015-03-17 19:42:52 +0000

    changed settings of test server to match actual server

--- testserver.js
+++ testserver.js
@@ -1,11 +1,17 @@
 exports.startserver = function() {
     var restify = require('restify');
     var server = restify.createServer();
-    var test = require('./routes.js');
-    test.getRoutes(server);
-    server.use(restify.bodyParser());
+    server.use(restify.CORS({
+    origins: ['*'], // defaults to ['*']
+    credentials: true, // defaults to false
+    headers: ['authorization', 'content-type', 'accept', 'origin'],
+    methods: ['GET', 'PUT', 'POST', 'HEAD', 'DELETE'] // sets expose-headers
+}));
+     server.use(restify.bodyParser());
     server.use(restify.queryParser());
     server.use(restify.authorizationParser());
+    var test = require('./routes.js');
+    test.getRoutes(server);
     console.log('server ' + server)
     server.listen('8080', 'localhost', function() {
         console.log('%s server listen at %s', server.name, server.url);
