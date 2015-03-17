e504226f7b3b659ede7b9d7540ff49e327964ca7
reissefer <reissefer@gmail.com>
2015-03-17 19:06:31 +0000

    changed routing of test server

--- testserver.js
+++ testserver.js
@@ -3,9 +3,10 @@ exports.startserver = function() {
     var server = restify.createServer();
    
     var test = require('./routes.js');
-    test.getRoutes(server);
+    
      console.log ('server '+ server)
     server.listen('8080', 'localhost', function() {
+        test.getRoutes(server);
         console.log('%s server listen at %s', server.name, server.url);
     });
 }
