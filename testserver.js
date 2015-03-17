72ebdd9064cfbb5c5aa0c10dd74fd4a118685501
reissefer <reissefer@gmail.com>
2015-03-17 19:26:35 +0000

    changed server settings

--- testserver.js
+++ testserver.js
@@ -1,12 +1,13 @@
 exports.startserver = function() {
     var restify = require('restify');
     var server = restify.createServer();
-   
     var test = require('./routes.js');
-    
-     console.log ('server '+ server)
+    test.getRoutes(server);
+    server.use(restify.bodyParser());
+    server.use(restify.queryParser());
+    server.use(restify.authorizationParser());
+    console.log('server ' + server)
     server.listen('8080', 'localhost', function() {
-        test.getRoutes(server);
         console.log('%s server listen at %s', server.name, server.url);
     });
 }
