82b761976a5b64fb12ed497321cb3b99a097ac97
adam91holt <holta5@uni.coventry.ac.uk>
2015-03-26 00:59:52 +0000

    Fix spec

--- spec/localr.spec.js
+++ spec/localr.spec.js
@@ -250,51 +250,6 @@
                  })
              });
          })
-//          it("Be able to get a single group", function(done) {
-//             var url = 'http://localhost:8080/groups/testgroup';
-//              // getting the parameters
-//              var params = {
-//                  uri: url,
-//                  headers: {
-//                      authorization: getBasic('testbusiness', 'test'),
-//                      "content-type": "application/json"
-//                  },
-//              };
-//              request.get(params, function(error, response, body) {
-//                  expect(response.statusCode).toBe(200);
-//                  console.log(body)
-//                  if(error) {
-//                      expect(error.code).not.toBe('ECONNREFUSED');
-//                  }
-//                  done();
-//              }) 
-             
-             
-//          })
-         
-//          it("Be able to see all groups", function(done) {
-//              var url = 'http://localhost:8080/groups?competition=freshers';
-//              // getting the parameters
-//              var params = {
-//                  uri: url,
-//                  headers: {
-//                      authorization: getBasic('testuser', 'test'),
-//                      "content-type": "application/json"
-//                  },
-//              };
-//              request.get(params, function(error, response, body) {
-//                  expect(response.statusCode).toBe(200);
-//                  console.log(body)
-//                  expect(body.total_groups).toBe(2);
-//                  if(error) {
-//                      expect(error.code).not.toBe('ECONNREFUSED');
-//                  }
-//                  done();
-//              }) 
-             
-             
-             
-//          })
      })
      describe('Offers', function() {
          it("Be able to add an offer", function(done) {
@@ -328,7 +283,8 @@
              var params = {
                  uri: url,
                  headers: {
-                     authorization: getBasic('testuser', 'test')
+                     authorization: getBasic('testuser', 'test'),
+                     "content-type": "application/json"
                  },
              };
              request.get(params, function(error, response, body) {
@@ -345,7 +301,8 @@
              var params = {
                  uri: url,
                  headers: {
-                     authorization: getBasic('testuser', 'test')
+                     authorization: getBasic('testuser', 'test'),
+                     "content-type": "application/json"
                  },
              };
              request.get(params, function(error, response, body) {
@@ -356,7 +313,7 @@
                  done();
              })
          })
-         it("be able to reddem an offer", function(done) {
+         it("be able to redeem an offer", function(done) {
              var url = 'http://localhost:8080/business/offers/redeem';
              var doc = {
                  offerTitle: 'test offer - testbusiness'
@@ -383,7 +340,7 @@
              })
          })
          it("be able to check what offers youve redeemed", function(done) {
-             var url = 'http://localhost:8080/users/testuser';
+             var url = 'http://localhost:8080/users/get/testuser';
              var params = {
                  uri: url,
                  headers: {
@@ -393,7 +350,7 @@
              request.get(params, function(error, response, body) {
                  expect(response.statusCode).toBe(200);
                  body = JSON.parse(body);
-                 expect(body.transactions[0].checked_in_at).toBe("coventry")
+                 expect(body.transactions[0].checked_in_at).toBe("testbusiness")
                  expect(body.transactions[1].business_redeemed).toBe("testbusiness")
                  if(error) {
                      expect(error.code).not.toBe('ECONNREFUSED');
