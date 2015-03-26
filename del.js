3f0be3e9d2c1965c8c0c4aa7f27eccacf9788c77
adam91holt <holta5@uni.coventry.ac.uk>
2015-03-26 17:00:14 +0000

    Updated delete file to reflect changes for checking password

--- del.js
+++ del.js
@@ -6,108 +6,115 @@ var validateHTTP = require("./validateHTTP.js"),
     neo4j = require('node-neo4j'),
     Promise = require('promise'),
     pwdCheck = require('./passwordCheck.js');
-
-module.exports.del = (function(){
+module.exports.del = (function() {
     var db = new neo4j('http://localhost:7474');
     return {
-        deleteUser: function(req, res, next){
+        deleteUser: function(req, res, next) {
             var username = req.params.username,
                 url = 'http://localhost:5984/users/' + username,
                 password = req.authorization.basic.password,
                 mainBody = {};
-            if(username !== req.authorization.basic.username){
+            if(username !== req.authorization.basic.username) {
                 return next(new restify.UnauthorizedError("You do not have permission to edit this user!"))
             };
             pwdCheck.check(username, password, 'user').
             catch(function(err) {
                 return next(new restify.UnauthorizedError('Invalid username/password'));
+                credentials = false;
             }).then(function() {
-                return new Promise(function(resolve, reject) {
-                    request.get(url, function(err, response, body) {
-                        if(err) {
-                            reject(err)
+                if(credentials === true) {
+                    return new Promise(function(resolve, reject) {
+                        request.get(url, function(err, response, body) {
+                            if(err) {
+                                reject(err)
+                            };
+                            console.log('code ' + response.statusCode)
+                            if(body) {
+                                resolve({
+                                    response: response,
+                                    body: body
+                                })
+                            }
+                        })
+                    }).
+                    catch(function(err) {
+                        console.log("GET request error on couchDB document")
+                        return next(new restify.InternalServerError('Error communicating with CouchDB'));
+                    }).then(function(body) {
+                        if(body.response.statusCode === 200) {
+                            mainBody = JSON.parse(body.body),
+                            db.cypherQuery("START n = node(" + mainBody.nodeid + ") OPTIONAL MATCH (n)-->(r) delete n, r;", function(err, results) {
+                                console.log('DELETING NODE: ' + mainBody.nodeid + " & RELATIONSHIPS.");
+                                console.log(results)
+                                if(err) {
+                                    return next(new restify.InternalServerError('Cant Delete Neo4j node.'));
+                                };
+                            });
                         };
-                        console.log('code ' + response.statusCode)
-                        if(body) {
-                            resolve({
-                                response: response,
-                                body: body
-                            })
-                        }
+                    }).then(function() {
+                        request.del(url + "?rev=" + mainBody._rev, function(err, response) {
+                            if(err) {
+                                return next(new restify.InternalServerError('Cant delete CouchDB document.'));
+                            };
+                            res.send(202, "Deleted User!");
+                            res.end();
+                        });
                     })
-                })
-            }).catch(function(err) {
-                console.log("GET request error on couchDB document")
-                return next(new restify.InternalServerError('Error communicating with CouchDB'));
-            }).then(function(body){
-                if(body.response.statusCode === 200){
-                    mainBody = JSON.parse(body.body),
-                    db.cypherQuery("START n = node(" + mainBody.nodeid + ") OPTIONAL MATCH (n)-->(r) delete n, r;", function(err, results) {
-                        console.log('DELETING NODE: ' + mainBody.nodeid + " & RELATIONSHIPS.");
-                        console.log(results)
-                        if(err){
-                            return next(new restify.InternalServerError('Cant Delete Neo4j node.'));
-                        };
-                    });
-                };
-            }).then(function(){
-                request.del(url + "?rev=" + mainBody._rev, function (err, response) {
-                    if (err) {
-                        return next(new restify.InternalServerError('Cant delete CouchDB document.'));
-                    };
-                    res.send(202, "Deleted User!");
-                    res.end();
-                });
+                }
             })
         },
-        deleteBusiness: function(req, res, next){
+        deleteBusiness: function(req, res, next) {
             var businessname = req.params.businessname,
                 url = 'http://localhost:5984/business/' + businessname,
                 password = req.authorization.basic.password,
                 mainBody = {};
-            if(businessname !== req.authorization.basic.username){
+            if(businessname !== req.authorization.basic.username) {
                 return next(new restify.UnauthorizedError("You do not have permission to edit this user!"))
             };
             pwdCheck.check(businessname, password, 'user').
             catch(function(err) {
                 return next(new restify.UnauthorizedError('Invalid username/password'));
+                credentials = false;
             }).then(function() {
-                return new Promise(function(resolve, reject) {
-                    request.get(url, function(err, response, body) {
-                        if(err) {
-                            reject(err)
+                if(credentials === true) {
+                    return new Promise(function(resolve, reject) {
+                        request.get(url, function(err, response, body) {
+                            if(err) {
+                                reject(err)
+                            };
+                            console.log('code ' + response.statusCode)
+                            if(body) {
+                                resolve({
+                                    response: response,
+                                    body: body
+                                })
+                            }
+                        })
+                    }).
+                    catch(function(err) {
+                        console.log("GET request error on couchDB document")
+                        return next(new restify.InternalServerError('Error communicating with CouchDB'));
+                    }).then(function(body) {
+                        if(body.response.statusCode === 200) {
+                            mainBody = JSON.parse(body.body),
+                            db.cypherQuery("START n = node(" + mainBody.nodeid + ") OPTIONAL MATCH (n)-->(r) delete n, r;", function(err, results) {
+                                console.log('DELETING NODE: ' + mainBody.nodeid + " & RELATIONSHIPS.");
+                                console.log(results)
+                                if(err) {
+                                    return next(new restify.InternalServerError('Cant Delete Neo4j node.'));
+                                };
+                            });
                         };
-                        console.log('code ' + response.statusCode)
-                        if(body) {
-                            resolve({
-                                response: response,
-                                body: body
-                            })
-                        }
+                    }).then(function() {
+                        request.del(url + "?rev=" + mainBody._rev, function(err, response) {
+                            if(err) {
+                                return next(new restify.InternalServerError('Cant delete CouchDB document.'));
+                            };
+                            res.send(202, "Deleted Business!");
+                            res.end();
+                        });
                     })
-                })
-            }).catch(function(err) {
-                console.log("GET request error on couchDB document")
-                return next(new restify.InternalServerError('Error communicating with CouchDB'));
-            }).then(function(body){
-                if(body.response.statusCode === 200){
-                    mainBody = JSON.parse(body.body),
-                    db.cypherQuery("START n = node(" + mainBody.nodeid + ") OPTIONAL MATCH (n)-->(r) delete n, r;", function(err, results) {
-                        console.log('DELETING NODE: ' + mainBody.nodeid + " & RELATIONSHIPS.");
-                        console.log(results)
-                        if(err){
-                            return next(new restify.InternalServerError('Cant Delete Neo4j node.'));
-                        };
-                    });
-                };
-            }).then(function(){
-                request.del(url + "?rev=" + mainBody._rev, function (err, response) {
-                    if (err) {
-                        return next(new restify.InternalServerError('Cant delete CouchDB document.'));
-                    };
-                    res.send(202, "Deleted Business!");
-                    res.end();
-                });
+                }
             })
         },
         getRequest: function(url) {
