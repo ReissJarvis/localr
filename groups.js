fe42c65bea664f033011f43f68bffe6f432406e3
reissefer <reissefer@gmail.com>
2015-04-22 12:11:52 +0000

    Stringify back in

--- groups.js
+++ groups.js
@@ -109,7 +109,7 @@ module.exports.groups = (function() {
                                             };
                                             var params = {
                                                 uri: url,
-                                                body: doc
+                                                body: JSON.stringify(doc
                                             };
                                             resolve(params);
                                         }).then(function(params) {
